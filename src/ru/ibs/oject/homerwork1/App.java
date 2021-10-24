dd8e9368dff891f91f5c8359eaca54785de09c74
UinstonSad <mat_pas2xov@mail.ru>
2021-10-24 15:08:01 +0300

    Переименовал переменные в APP , изменил структуру List и Hash  и их объявление

--- src/ru/ibs/oject/homerwork1/App.java
+++ src/ru/ibs/oject/homerwork1/App.java
@@ -4,28 +4,31 @@ package ru.ibs.oject.homerwork1;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class App {
     public static void main(String[] string){
-        Developer Ivan = new Developer("Игра",25,3,true);
-        Ivan.setName("Иван");
-        Ivan.setAge(25);
-        Ivan.setSalary(60000);
-        Intern Matvey=new Intern("Матвей","Иван", 22, 35000,2.5f);
-        TeamLeader Kolia=new TeamLeader("Коля", 10,5,35,12000);
-        Ivan.setAge(30);
-//        System.out.println(Ivan.getAge());
-//        System.out.println(Ivan.getName());
-//        Ivan.performance("Иван","Игра", true);
+        Developer ivan = new Developer("Игра",25,3,true);
+        ivan.setName("Иван");
+        ivan.setAge(25);
+        ivan.setSalary(60000);
+        Intern matvey=new Intern("Матвей","Иван", 22, 35000,2.5f);
+        TeamLeader kolia=new TeamLeader("Коля", 10,5,35,12000);
+        ivan.setAge(30);
+//        System.out.println(ivan.getAge());
+//        System.out.println(ivan.getName());
+//        ivan.performance("Иван","Игра", true);
 //
-//        Ivan.welcome("Иван");
-//        Matvey.welcome("Матвей");
-//        Kolia.welcome("Коля");
+//        ivan.welcome("Иван");
+//        matvey.welcome("Матвей");
+//        kolia.welcome("Коля");
 
-        HashSet<Employee> employees= new HashSet<>();
-        employees.add(Ivan);
-        employees.add(Matvey);
-        employees.add(Kolia);
+       // HashSet<Employee> employees= new HashSet<>();
+        Set<Employee> employees= new HashSet<>();
+        employees.add(ivan);
+        employees.add(matvey);
+        employees.add(kolia);
         System.out.println(employees);
         for(Employee e : employees){
             System.out.println(e.getClass());
@@ -35,21 +38,20 @@ public class App {
         }
 
 
-        ArrayList<Employee> employeeArrayList=new ArrayList<>();
-        for(Employee e : employees){
-            employeeArrayList.add(e);
-        }
+        //ArrayList<Employee> employeeArrayList = new ArrayList<>(employees);
+        List<Employee> employeeArrayList=new ArrayList<>(employees);
+
         System.out.println(employeeArrayList.get(0));
         System.out.println(employeeArrayList.get(1));
 
 
         System.out.println(employeeArrayList.get(1).getName());
-        for(int e=0;e<employeeArrayList.size() ; e++ ){
+        for (Employee employee : employeeArrayList) {
 
-            System.out.println(employeeArrayList.get(e).getClass());
-            System.out.println(employeeArrayList.get(e).getName());
-            System.out.println(employeeArrayList.get(e).getAge());
-            System.out.println(employeeArrayList.get(e).getSalary());
+            System.out.println(employee.getClass());
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+            System.out.println(employee.getSalary());
         }
 
 
